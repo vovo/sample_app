9c2a89a4fe678491f6454296ae130105214724a3
Vovo Yang <vovo.yang@gmail.com>
2016-06-10 05:51:28 +0000

    refactoring application title code

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Tutorial Sample App"
+    assert_select "title", "Tutorial Sample App"
   end
 
   test "should get help" do
