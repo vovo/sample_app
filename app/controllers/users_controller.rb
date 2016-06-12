5c6faf41f1bec9277b9685b6fb89629ed853785c
Vovo Yang <vovo.yang@gmail.com>
2016-06-12 18:04:48 +0000

    redirect when signup succeeded

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # handle a successful save
+      redirect_to @user
     else
       render 'new'
     end
