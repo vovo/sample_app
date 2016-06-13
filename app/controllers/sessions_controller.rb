3813184930db11d4a452b8bfba6f716cfbfb9993
Vovo Yang <vovo.yang@gmail.com>
2016-06-13 16:10:34 +0000

    session#create v1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in, redirect to user's show page
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
