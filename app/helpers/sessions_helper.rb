7067cb6036a3e82df760100a392d41a367b99a48
Vovo Yang <vovo.yang@gmail.com>
2016-06-14 17:59:32 +0000

    redirect user edit/update when logged in as wrong user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,11 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token    
   end
 
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
   # Returns the current logged-in user (if any).
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
