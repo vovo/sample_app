044247ca462bdd4d49612d48eca8a62438b2ebb3
Vovo Yang <vovo.yang@gmail.com>
2016-06-14 07:21:31 +0000

    fix db field name: users.remember_digest

--- db/migrate/20160614053723_add_remember_digest_to_users.rb
+++ db/migrate/20160614053723_add_remember_digest_to_users.rb
@@ -1,5 +1,5 @@
 class AddRememberDigestToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :remmember_digest, :string
+    add_column :users, :remember_digest, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,10 @@ ActiveRecord::Schema.define(version: 20160614053723) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "password_digest"
-    t.string   "remmember_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
