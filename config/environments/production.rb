160cde1ec0d8659c8c788e3a32cb8d7865461f87
Vovo Yang <vovo.yang@gmail.com>
2016-06-18 05:15:25 +0000

    fix production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,8 +67,8 @@ Rails.application.configure do
   host = 'sample452.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net'
-    :port           => '587'
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
