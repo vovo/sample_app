13af592f23522cf879ccd5ece1024ac2f1c7c8cb
Vovo Yang <vovo.yang@gmail.com>
2016-06-18 14:00:24 +0000

    micropost refinements

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -79,4 +79,13 @@ class UserTest < ActiveSupport::TestCase
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
   end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lonely cat")
+    assert_difference 'Micropost.count' , -1 do
+      @user.destroy
+    end
+  end
+
 end
