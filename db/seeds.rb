661538010bc60ec642ffab89ee7d0f4b66657880
Vovo Yang <vovo.yang@gmail.com>
2016-06-18 15:02:23 +0000

    render microposts on user profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,35 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-def create_user(name, email, password, admin=false)
-  User.create!(name: name,
-               email: email,
-               password:              password,
-               password_confirmation: password,
-               admin: admin,
-               activated: true,
-               activated_at: Time.zone.now)
+if User.count == 0
+  def create_user(name, email, password, admin=false)
+    User.create!(name: name,
+                 email: email,
+                 password:              password,
+                 password_confirmation: password,
+                 admin: admin,
+                 activated: true,
+                 activated_at: Time.zone.now)
+  end
+
+  create_user("vv", "vv@vv.com", "aaaaaa", true)
+  create_user("Example User",
+              "example@railstutorial.org",
+              "foobar")
+
+  99.times do |n|
+    name = Faker::Name.name
+    email = "example-#{n+1}@railstutorial.org"
+    password = "password"
+    create_user(name, email, password)
+  end
 end
 
-create_user("vv", "vv@vv.com", "aaaaaa", true)
-create_user("Example User",
-            "example@railstutorial.org",
-            "foobar")
-
-99.times do |n|
-  name = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  create_user(name, email, password)
+if Micropost.count == 0
+  users = User.order(:created_at).take(6)
+  50.times do
+    content = Faker::Lorem.sentence(5)
+    users.each { |user| user.microposts.create!(content: content) }
+  end
 end
-  
+
