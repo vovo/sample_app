1e1c75e6704f24a2f30ca26719bea709d7723ad8
Vovo Yang <vovo.yang@gmail.com>
2016-06-20 12:09:14 +0000

    status feed v2: more efficient select

--- app/models/user.rb
+++ app/models/user.rb
@@ -77,10 +77,12 @@ class User < ActiveRecord::Base
     reset_sent_at < 2.hours.ago
   end
 
-  # Defines a proto-feed.
-  # See "Following users" for the full implementation
+  # Defines a user's status feed.
   def feed
-    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
   # Follows a user.
